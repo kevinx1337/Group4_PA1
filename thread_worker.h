3cf6fe088e3a5d0f9fdd34b64f63c1e2da8dd876
jwilliams231 <JimmyWilliamsK@gmail.com>
2026-09-13 16:25:15 -0700

    Add thread command execution functions
    Added function prototypes for commandThread and executeCommandInThread.

--- thread_worker.h
+++ thread_worker.h
@@ -2,3 +2,10 @@
 #define THREAD_WORKER_H
 
 #include "myshell.h"
+
+void *commandThread(void *argument);
+
+//rund one validated command on child thread
+int executeCommandInThread(const ParsedCommand *command);
+
+#endif
